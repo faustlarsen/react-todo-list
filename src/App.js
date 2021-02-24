ec5cfce0191f51066cf78bbb454dfa5da7453392
Constantine <faustlarsen@gmail.com>
2021-02-23 20:37:58 -0700

    create useEffect hook to store data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react;'
+import React, { useEffect, useState } from 'react;'
 import './App.css';
 import TodoForm from "./components/TodoForm";
-// import TodoList from "./components/TodoList";
+import TodoList from "./components/TodoList";
+
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
 
 
 function App() {
@@ -11,6 +13,11 @@ function App() {
  function addTodo(todo) {
    setTodos([todo, ...todos]);
  }
+
+ useEffect(() => {
+  // fires when todos array gets updated
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+}, [todos]);
 
   return (
     <div className="App">
@@ -23,3 +30,19 @@ function App() {
 }
 
 export default App;
+
+
+// useeffect(() => {
+//   //effect
+//   return () => {
+//     //clean up
+//   }
+// }, [dependecyArray]);
+// 1. it takes 2 parameters - function which is a useEffect, 
+// and dependecyArray
+// 2. dependecyArray is a parameter that gets used to determine
+// is the effects is fired off or not.
+// 3.  if 1 or more of the variables in the dependecyArray changes,
+// the effect will be fired.
+// 4. if dependencyArray is empty - then the effect will be fired when 
+// the Component is initially rendered
